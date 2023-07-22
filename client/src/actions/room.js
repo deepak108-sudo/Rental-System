a812074188741bcec504715120bcaa8ebed2f49d
Deepak <deepak977140@gmail.com>
2023-07-22 14:49:49 +0530

    3rd commit(added succefully)

--- client/src/actions/room.js
+++ client/src/actions/room.js
@@ -9,7 +9,7 @@ export const createRoom= async (room,currentUser, dispatch, setPage)=>{
     const result = await fetchData({url, body:room, token:currentUser?.token}, dispatch);
     if(result){
         dispatch({type:'UPDATE_ALERT', payload:{
-            open:true,severity:'success',message:'The room has been addes successfully'},
+            open:true,severity:'success',message:'The room has been added successfully'},
         });
         dispatch({type:'RESET_ROOM'})
         setPage(0);
